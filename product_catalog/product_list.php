e8f6a0e4c86c4f8302481d47773c517fc049717e
A Sharma <as3426@njit.edu>
2017-07-09 16:30:53 -0400

    refactored with categories_nav.php

--- product_catalog/product_list.php
+++ product_catalog/product_list.php
@@ -2,19 +2,10 @@
 <main>
     <aside>
         <h1>Categories</h1>
-        <nav>
-        <ul>
-            <!-- display links for all categories -->
-            <?php foreach($categories as $category) : ?>
-            <li>
-                <a href="?category_id=<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
-                </a>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-        </nav>
-    </aside>
+<?php
+include('categories_nav.php');
+?>
+</aside>
     <section>
         <h1><?php echo $category_name; ?></h1>
         <ul class="nav">
